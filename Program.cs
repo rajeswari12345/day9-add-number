9d882d788f022943c9401b0d14168762054f1dc2
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-03 10:48:43 +0530

    program add number

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    static void Main(string[] args)
+    { 
+       
+     Person person = new Person();
+    Console.Write("Enter First Name: ");
+    person.FirstName = Console.ReadLine();
+
+    Console.Write("Enter Last Name: ");
+    person.LastName = Console.ReadLine();
+
+    Console.Write("Enter Phone Number: ");
+    person.PhoneNumber = Console.ReadLine();
+
+    Console.Write("Enter address: ");
+    person.address = Console.ReadLine();
+
+    Console.Write("Enter city: ");
+    person.city = Console.ReadLine();
+
+    Console.Write("Enter state: ");
+    person.state = Console.ReadLine();
+
+    Console.Write("Enter mail id: ");
+    person.mail id = Console.ReadLine();
+
+    Console.Write(" Enter zip: ");
+    person.zip = Console.ReadLine();
+}
