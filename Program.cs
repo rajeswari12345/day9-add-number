06af4f7eb3dfd5e9c347149d9df337615a65526e
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-03 11:58:17 +0530

    day 9 4c 4

--- Program.cs
+++ Program.cs
@@ -1,31 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 class program
 {
     static void Main(string[] args)
-    { 
-       
-     Person person = new Person();
-    Console.Write("Enter First Name: ");
-    person.FirstName = Console.ReadLine();
+    {
 
-    Console.Write("Enter Last Name: ");
-    person.LastName = Console.ReadLine();
+        Person person = new Person();
+        Console.Write("Enter your Name: ");
+        person.Name = Console.ReadLine();
 
-    Console.Write("Enter Phone Number: ");
-    person.PhoneNumber = Console.ReadLine();
+        Console.Write("name is mani ");
+        person.name = Console.ReadLine();
 
-    Console.Write("Enter address: ");
-    person.address = Console.ReadLine();
-
-    Console.Write("Enter city: ");
-    person.city = Console.ReadLine();
-
-    Console.Write("Enter state: ");
-    person.state = Console.ReadLine();
-
-    Console.Write("Enter mail id: ");
-    person.mail id = Console.ReadLine();
-
-    Console.Write(" Enter zip: ");
-    person.zip = Console.ReadLine();
+        Console.Write("delete you name: ");
+        person.Name = Console.ReadLine();
+    }
 }
+
+
+
+
